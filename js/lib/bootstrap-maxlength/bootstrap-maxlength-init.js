c8c42b877e666661689381e08f98fa9ef1ad4453
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2016-07-25 17:04:32 -0300

    Ajustes em todas as telas: Remoção do angular; Reajustes dos includes; Reajustes no csss's;

--- js/lib/bootstrap-maxlength/bootstrap-maxlength-init.js
+++ js/lib/bootstrap-maxlength/bootstrap-maxlength-init.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-    $('input.maxlength-simple').maxlength();
-
-    $('input.maxlength-custom-message').maxlength({
-        threshold: 10,
+    $('.maxlength-custom-message').maxlength({
+        threshold: 20,
         warningClass: "label label-success",
         limitReachedClass: "label label-danger",
-        separator: ' of ',
-        preText: 'You have ',
-        postText: ' chars remaining.',
-        validate: true
-    });
-
-    $('input.maxlength-always-show').maxlength({
-        alwaysShow: true
-    });
-
-    $('textarea.maxlength-simple').maxlength({
+        separator: ' de ',
+        preText: 'Você já utilizou ',
+        postText: ' caracteres disponíveis.',
+        validate: true,
         alwaysShow: true
     });
 });
