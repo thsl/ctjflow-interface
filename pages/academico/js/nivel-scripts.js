5b9bb4704515b47a33c471e9fbceecc2ec7c4121
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2016-08-01 17:45:31 -0300

    Ajustes em cursos

--- pages/academico/js/nivel-scripts.js
+++ pages/academico/js/nivel-scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     // Mascaramento dos campos
     $('#qtd-uni-avaliativas').mask('0000');
+    $('#qtdunav-busca').mask('0000');
 
     // Validador de campos
     $("#nivel-form").validate({
